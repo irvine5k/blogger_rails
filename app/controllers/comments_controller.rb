938c7b5b12d29f2de01c01a9ac8a7e8e351bee09
Kelven <kelvengalvao@gmail.com>
2019-02-23 16:39:50 -0300

    Comments feature done

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.article_id = params[:article_id]
+        if @comment.save
+          flash[:success] = "Object successfully created"
+          redirect_to article_path(@comment.article)
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    private 
+
+    def comment_params 
+        params.require(:comment).permit(:author, :body)
+    end
+    
+end
